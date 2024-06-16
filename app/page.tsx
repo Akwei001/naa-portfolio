2f5ab0a416e4f5ce92e276e6f8e35d6f80a96101
Akwei001 <68909965+Akwei001@users.noreply.github.com>
2024-06-16 17:56:56 +0100

    bento grid init

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import { FloatingNav } from "../components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
 import Hero from "../components/Hero";
+import Grid from "../components/Grid";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <FloatingNav navItems={[{name: 'Home', link : '/', icon: <FaHome/>}]
       }/>
       <Hero/>
+      <Grid/>
      </div>
     </main>
   );
